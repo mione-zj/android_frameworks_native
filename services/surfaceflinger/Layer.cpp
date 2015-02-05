6333efae993f36b09a7c5b8b4324d55b2aa9da55
Diogo Ferreira <defer@cyngn.com>
2015-02-05 17:07:10 +0000

    surfaceflinger: Allow MTK platforms to render level-3 DRM with the GPU
    This allows mediatek devices to specify a property that lets devices
    provisioned with level-3 DRM to render protected layers using the
    GPU if the persist.gralloc.cp.level3 property is set to 1.
    
    Change-Id: Id922e8c1ff13cc65af179588e8294a1a3483e975

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -717,7 +717,7 @@ void Layer::onDraw(const sp<const DisplayDevice>& hw, const Region& clip,
     }
 
     bool canAllowGPU = false;
-#ifdef QCOM_BSP
+#if defined(QCOM_BSP) || defined(MTK_HARDWARE)
     if(isProtected()) {
         char property[PROPERTY_VALUE_MAX];
         if ((property_get("persist.gralloc.cp.level3", property, NULL) > 0) &&
